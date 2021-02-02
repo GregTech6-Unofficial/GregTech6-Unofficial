b576539fd888c8b698509d67aeba2e492d5c4e47
TeinFanshu <GTNW+TeinFanshu@users.noreply.github.com>
2021-02-02 21:56:11 +0800

    Light Power Sensor

--- src/main/java/gregtech/tileentity/energy/generators/MultiTileEntitySPP.java
+++ src/main/java/gregtech/tileentity/energy/generators/MultiTileEntitySPP.java
@@ -14,7 +14,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with gregtech6. If not, see <http://www.gnu.org/licenses/>.
+ * along with GregTech6-Unofficial. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package gregtech.tileentity.energy.generators;
